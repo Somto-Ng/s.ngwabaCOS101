12bd217cf10e4cfd88c7bd20ba16607906c1fc16
Somto-Ng <somtochukwu.ngwaba@pau.edu.ng>
2025-11-24 10:03:34 +0100

    (Week 9)

---
+++ week-9/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\New User\Documents\s.ngwabaCOS101\week-9\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\New User\Documents\s.ngwabaCOS101\week-9\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\New\ User\Documents\s.ngwabaCOS101\week-9\practice_2\target\debug\practice_2.exe: C:\Users\New\ User\Documents\s.ngwabaCOS101\week-9\practice_2\src\main.rs
